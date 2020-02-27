7d991a4ae63a7fa32cea1c0f8333b06d42612862
snow treetree <18817834076@163.com>
2020-02-27 17:37:42 +0800

    feat: 初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    themeConfig: {
+    // 顶部导航
+      nav: [
+        { text: 'Home',  ariaLabel: 'Language Menu',link: '/', },
+        { text: 'Guide', link: '/guide/', items: [
+            { text: 'Chinese', link: '/language/chinese/' },
+            { text: 'Japanese', link: '/language/japanese/' }
+          ] },
+      ],
+      sidebar:[
+          {
+            title:'常用',
+            path:'/',
+            children:[
+                '/EventLoop'
+            ]
+        },
+        {
+            title:'JavaScript',
+            path:'/',
+            children:[
+                '/scroll'
+            ]
+        },
+        {
+          title:'其他',
+          path:'/',
+          children:[
+              '/other/select'
+          ]
+      },
+          '/EventLoop',
+      ]
+    }
+  }
